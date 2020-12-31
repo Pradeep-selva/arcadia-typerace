d827a13696abc897ec1503f7fb58fbac89456b98
pradeep-selva <pradeepselvaraju@gmail.com>
2021-01-01 05:00:36 +0530

    validation page functionality complete

--- client-ui/src/views/Validate.vue
+++ client-ui/src/views/Validate.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class="about">
-    <h1>Validate</h1>
-    <br />
-    <h3>{{ roomId }} {{ valType }}</h3>
-  </div>
+  <v-container
+    class="validate
+    d-flex
+    flex-column 
+    justify-center 
+    align-center"
+  >
+    <v-progress-circular
+      :size="150"
+      color="primary"
+      indeterminate
+      class="mb-16"
+    />
+    <h1 v-if="loading" class="grey--text text--lighten-2">
+      Validating your request...
+    </h1>
+    <h1 v-if="!!error" class="grey--text text--lighten-2">{{ error }}</h1>
+  </v-container>
 </template>
 
 <script lang="ts">
-import { VAL_TYPES } from "@/configs";
+import { ROUTES, VAL_TYPES } from "@/configs";
 import { Vue, Component } from "vue-property-decorator";
 import { validateOperation } from "../apis";
 
@@ -15,11 +28,34 @@ import { validateOperation } from "../apis";
 export default class Validate extends Vue {
   valType = this.$route.params.valType;
   roomId = this.$route.params.roomId;
+  error = "";
+  loading = false;
 
-  mounted() {
-    validateOperation(this.valType as keyof typeof VAL_TYPES, this.roomId).then(
-      console.log
-    );
+  async mounted() {
+    this.loading = true;
+
+    try {
+      const response = await validateOperation(
+        this.valType as keyof typeof VAL_TYPES,
+        this.roomId
+      );
+
+      if (response.ok)
+        this.$router.push({
+          path: ROUTES.room(this.roomId)
+        });
+      else this.error = response.data;
+    } catch (error) {
+      this.error = error;
+    } finally {
+      this.loading = false;
+    }
   }
 }
 </script>
+
+<style>
+.validate {
+  height: 100vh;
+}
+</style>
