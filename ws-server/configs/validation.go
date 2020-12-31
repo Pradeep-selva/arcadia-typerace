78b7a0cb59b685a1d34e8773cd3ef3ab30fd89aa
pradeep-selva <pradeepselvaraju@gmail.com>
2021-01-01 05:08:47 +0530

    validation message changed

--- ws-server/configs/validation.go
+++ ws-server/configs/validation.go
@@ -10,11 +10,19 @@ var validationTypes = struct {
 }{"join", "create"}
 
 func GetIfRequiredUsersPresent(valType string, users int) error {
+	message := ""
+
+	if valType == validationTypes.JOIN || valType == validationTypes.CREATE {
+		message = "Unable to "+valType+" room. Please go to home."
+	} else {
+		message = "Invalid operation encountered. Please go to home."
+	}
+
 	if valType == validationTypes.JOIN &&  users == 1 {
 		return nil
 	} else if valType == validationTypes.CREATE && users == 0 {
 		return nil
 	} else {
-		return fmt.Errorf("Unable to "+valType+" room.")
+		return fmt.Errorf(message)
 	}
 }
